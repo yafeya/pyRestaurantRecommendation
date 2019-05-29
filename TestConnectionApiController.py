f4aa1ffaae15d2ae0f73930dcde0192072a59b00
Feiyang_Yang <Feiyang_Yang@dell.com>
2019-05-29 11:06:15 +0800

    add index controler

--- TestConnectionApiController.py
+++ TestConnectionApiController.py
@@ -2,6 +2,10 @@ from flask_restful import Resource
 
 
 class TestConnectionApiController(Resource):
-
     def get(self):
         return 'You are connected', 200
+
+
+class IndexConnectionApiController(Resource):
+    def get(self):
+        return 'Restaurant Service', 200
--- program.py
+++ program.py
@@ -1,12 +1,29 @@
-from rest_server import start_restful_server
 import json
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+from RestaurantApiControllers import RestaurantsInAreaApiController
+from TestConnectionApiController import TestConnectionApiController, IndexConnectionApiController
+
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+app.config['SECRET_KEY'] = 'secret!'
+
+
+def start_restful_server():
+    ak = 'jLKWXCmDwGdfddhBvaB0GmqBr8K5gwum'
+    sk = 'ZphXAtI0goU2aRcOGFpzPsWmZOY00UNa'
+    print('starting restful server for restaurant recommendation...')
+    restaurant_args = {'ak': ak, 'sk': sk}
+    api.add_resource(RestaurantsInAreaApiController, '/restaurants/<location>/<price_section>',
+                     resource_class_kwargs=restaurant_args)
+    api.add_resource(TestConnectionApiController, '/test')
+    api.add_resource(IndexConnectionApiController, '/')
+    app.run()
 
 
 if __name__ == '__main__':
-    settings: dict
-    with open("settings.json", 'r') as settings_file:
-        settings = json.load(settings_file)
-    host = settings.get('host')
-    port = int(settings.get('port'))
-
     start_restful_server()
